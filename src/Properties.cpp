82d9ac6989fc345d83fcaa11c94d64042f88b41d
PerciseParrot <perciseparrot@gmail.com>
2024-11-30 11:19:16 +0100

    Fixed crash when accessing properties

--- src/Properties.cpp
+++ src/Properties.cpp
@@ -2,21 +2,25 @@
 
 bool Properties::getBool(const std::string& index) const
 {
+    if (!propertiesBool.contains(index)) return false;
     return propertiesBool.at(index);
 }
 
 float Properties::getFloat(const std::string& index) const
 {
+    if (!propertiesFloat.contains(index)) return 0.0f;
     return propertiesFloat.at(index);
 }
 
 int Properties::getInt(const std::string& index) const
 {
+    if (!propertiesInt.contains(index)) return 0;
     return propertiesInt.at(index);
 }
 
 std::string Properties::getString(const std::string& index) const
 {
+    if (!propertiesString.contains(index)) return "";
     return propertiesString.at(index);
 }
 
